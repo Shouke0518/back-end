f2e32e0439524144000482f30c84e478a9d2ebcf
O Ko TuTuUTa <shit141414@gmail.com>
2024-12-15 00:50:49 +0800

    render adjust

--- database/index.js
+++ database/index.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
 mongoose
-    .connect("mongodb+srv://shit141414:ob7WBFD7nZJWC50Q@informationswitch.eghn1.mongodb.net/?retryWrites=true&w=majority&appName=informationSwitch")
+    .connect("mongodb+srv://shit141414:ob7WBFD7nZJWC50Q@informationswitch.eghn1.mongodb.net/?retryWrites=true&w=majority&appName=informationSwitch", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ssl: true,
+        tls: true,
+        tlsAllowInvalidCertificates: false,
+        retryWrites: true
+    })
     .then(() => console.log('Connected to DB'))
     .catch((err) => console.log(err));
