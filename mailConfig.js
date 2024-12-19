cdd98b119622116703b359c44cb6a223ae646568
O Ko TuTuUTa <shit141414@gmail.com>
2024-12-19 20:36:08 +0800

    adjust render

--- mailConfig.js
+++ mailConfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    MAIL_CLIENT_ID: "49051616591-53cse0t4fcepfsrkhr2kkj98f46md7te.apps.googleusercontent.com",
-    MAIL_CLIENT_SECRET: "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",
-    MAIL_REFRESH_TOKEN: "1//04OBkNo2EBYDgCgYIARAAGAQSNwF-L9IrfKTWqZ8IbTXNVEX4DroiDYm0Uz7XSgbnQmZ7ujKw-o7TbomRaEjBm6mI7oQg7VrNtUc",
-    EMAIL: "h279425@gmail.com"
+    MAIL_CLIENT_ID: "",
+    MAIL_CLIENT_SECRET: "",
+    MAIL_REFRESH_TOKEN: "",
+    EMAIL: ""
 }
